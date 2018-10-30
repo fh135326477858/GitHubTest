2309ef29abcc08f0b64affcafe8dbd481fe5b13e
ahui <fh13526477858@gmail.com>
2018-10-30 19:49:51 +0800

    this is eclipse git commit

---
+++ src/hg/test/git/GitA.java
@@ -0,0 +1,7 @@
+package hg.test.git;
+
+public class GitA {
+	public static void main(String[] args) {
+		System.out.println("asaaa");
+	}
+}
---
+++ src/hg/test/git/GitB.java
@@ -0,0 +1,8 @@
+package hg.test.git;
+
+public class GitB {
+	private void mian() {
+		// TODO Auto-generated method stub
+		System.out.println("bbbbb");
+	}
+}
